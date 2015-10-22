da058a29f51e1f13d54cefafb78a82fac659bff4
ZQY <sunnyuny8@gmail.com>
2015-10-21 17:18:16 -0700

    format code in devise_helper

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,14 +2,14 @@
 module DeviseHelper
   def devise_error_messages!
     return '' if resource.errors.empty?
-    
+
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    html = <<-HTML
-    <div class="alert alert-error alert-danger"> <button type="button"
-    class="close" data-dismiss="alert">x</button>
-      #{messages}
-    </div>
-    HTML
+    html =
+      <<-HTML
+        <div class="alert alert-error alert-danger">
+          #{messages}
+        </div>
+      HTML
 
     html.html_safe
   end
