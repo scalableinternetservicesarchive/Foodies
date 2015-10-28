eaa84f94adea27d223bafc729c172ada83a9ccfd
katiesl <anvincy@gmail.com>
2015-10-27 17:45:32 -0700

    user could save a recipe [Finishes #106528692]

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   resources :masterpieces
   resources :recipes
-  post '/recipes/:id' => 'recipes#like'
-  post '/masterpieces/:id' => 'masterpieces#like'
+  post '/recipes/:id/like' => 'recipes#like'
+  post '/masterpieces/:id/like' => 'masterpieces#like'
   resources :profile
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  post '/recipes/:id/bookmark' => 'recipes#bookmark'
   # You can have the root of your site routed with "root"
   root 'recipes#index'
 
