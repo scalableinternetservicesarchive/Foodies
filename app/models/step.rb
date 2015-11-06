58a6055c131ee43a59ec4683d3654f249180f667
muxin <muxin4ever@gmail.com>
2015-11-06 06:21:10 -0800

    Better search view and profile view - better masterpiece search result - better profile tabs including recipe image and masterpiece image

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,9 +2,8 @@ class Step < ActiveRecord::Base
   belongs_to :recipe
   validates :step_number, :description, presence: true
   has_attached_file :step_img, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
+    thumb: '100x100#',
+    square: '200x200#'
   }, default_url: "/images/default/missing_:attachment_:style.jpg"
   validates_attachment_content_type :step_img, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
