7a4c452154d8a088ea533b7847f12a7cb4473d1d
katiesl <anvincy@gmail.com>
2015-11-12 22:29:27 -0800

    User can access profile page via /profile/id User can edit avatar

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -8,4 +8,29 @@ class ProfileController < ApplicationController
     @cooked_masterpieces = current_user.cooked_masterpieces
     @liked_masterpieces = current_user.liked_masterpieces
   end
+
+  def show
+    @user = User.find(params[:id])
+    @created_recipes = @user.created_recipes
+    @bookmarked_recipes = @user.bookmarked_recipes
+    @liked_recipes = @user.liked_recipes
+    @cooked_masterpieces = @user.cooked_masterpieces
+    @liked_masterpieces = @user.liked_masterpieces
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def profile_params
+      params.require(:profile).permit(:id)
+    end
+
 end
