02fbafb4a59056a5b58a61e59a26dadb79f93b00
zjciris <zjc9211@gmail.com>
2015-11-12 22:30:05 -0800

    search bug fixed [Finishes #107618516]

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,16 +7,8 @@ def find_by_recipe
   @recipes = Recipe.search(params[:search]).order("created_at DESC")
 end
 
-def find_by_masterpiece
-  @masterpieces = Masterpiece.search(params[:search]).order("created_at DESC")
-end
-
 def find_by_user
   @users = User.search(params[:search]).order("email ASC")
-end
-
-def find_by_ingredient
-  @ingredients = Ingredient.search(params[:search]).select(:name).distinct.order("name ASC")
 end
 
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,4 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
 
-  def self.search(search)
-    search_condition = "%"+search+"%"
-    where("name LIKE ?  ",  search_condition)
-  end
 end
