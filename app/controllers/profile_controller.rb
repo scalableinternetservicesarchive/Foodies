8af9458e8f14aa01c547c52536cbc83cedd8430f
katiesl <anvincy@gmail.com>
2015-11-13 21:39:20 -0800

    Delete profile index page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,14 +1,4 @@
 class ProfileController < ApplicationController
-  before_filter :authenticate_user!
-  def index
-    @user = current_user
-    @created_recipes = current_user.created_recipes
-    @bookmarked_recipes = current_user.bookmarked_recipes
-    @liked_recipes = current_user.liked_recipes
-    @cooked_masterpieces = current_user.cooked_masterpieces
-    @liked_masterpieces = current_user.liked_masterpieces
-  end
-
   def show
     @user = User.find(params[:id])
     @created_recipes = @user.created_recipes
@@ -16,12 +6,6 @@ class ProfileController < ApplicationController
     @liked_recipes = @user.liked_recipes
     @cooked_masterpieces = @user.cooked_masterpieces
     @liked_masterpieces = @user.liked_masterpieces
-  end
-
-  def new
-  end
-
-  def edit
   end
 
   private
@@ -32,5 +16,4 @@ class ProfileController < ApplicationController
     def profile_params
       params.require(:profile).permit(:id)
     end
-
 end
