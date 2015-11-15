46ef5afe2a1e951963bac114980477936c47199d
muxin <muxin4ever@gmail.com>
2015-11-14 23:27:32 -0800

    Turn off CSRF protection for Tsung instances

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   class SecurityTransgression < StandardError
   end
