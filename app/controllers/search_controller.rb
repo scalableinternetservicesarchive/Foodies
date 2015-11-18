18185cec77c46a82c203dfa3bba7332108eb26b4
muxin <muxin4ever@gmail.com>
2015-11-18 00:39:19 -0800

    Fix a lot of html bugs in search page and tag page - fix unclosed html tags problem - reformat code - display search keyword in search result

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,10 +4,12 @@ def index
 end
 
 def find_by_recipe
+  @keyword = params[:search]
   @recipes = Recipe.search(params[:search]).order("created_at DESC")
 end
 
 def find_by_user
+  @keyword = params[:search]
   @users = User.search(params[:search]).order("email ASC")
 end
 
