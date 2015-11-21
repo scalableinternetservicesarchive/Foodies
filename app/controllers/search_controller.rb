812d36ee7dc30a0efc0c4a04e1dee4be04797f3d
muxin <muxin4ever@gmail.com>
2015-11-20 23:32:26 -0800

    SQL Optimization using raw SQL - use raw sql in search/find_by_recipe to get recipes including count info - use raw sql in search/find_by_tag to get recipes including count info

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,38 @@ def find_by_recipe
   if @keyword == ''
     @recipes = Recipe.none
   else
-    @recipes = Recipe.search(params[:search]).order("created_at DESC")
+    # @recipes = Recipe.search(params[:search]).order("created_at DESC")
+
+    @recipes = Recipe.find_by_sql(["
+    SELECT recipes_with_keyword.* , 
+       COALESCE( like.cnt, 0 ) AS users_liking_it_count,
+       COALESCE( bookmark.cnt, 0 ) AS users_bookmarking_it_count,
+       COALESCE( cook.cnt, 0 ) AS cooks_count
+    FROM (
+      SELECT * FROM
+      recipes
+      WHERE title LIKE ? OR description LIKE ? 
+      ORDER BY created_at DESC
+    ) AS recipes_with_keyword
+    LEFT JOIN 
+      ( SELECT recipe_id, COUNT(*) AS cnt FROM
+        user_like_recipes
+        GROUP BY recipe_id ) AS like
+    ON recipes_with_keyword.id = like.recipe_id
+    LEFT JOIN
+      ( SELECT recipe_id, COUNT(*) AS cnt FROM
+        user_bookmark_recipes
+        GROUP BY recipe_id ) AS bookmark
+    ON recipes_with_keyword.id = bookmark.recipe_id
+    LEFT JOIN
+      ( SELECT recipe_id, COUNT(*) AS cnt FROM
+        masterpieces
+        GROUP BY recipe_id ) AS cook
+    ON recipes_with_keyword.id = cook.recipe_id
+    ", "%"+@keyword+"%", "%"+@keyword+"%"])
+
+    # ActiveRecord::Associations::Preloader.new.preload(@recipes, :tags)
+    # ActiveRecord::Associations::Preloader.new.preload(@recipes, :creator)
   end
 end
 
@@ -17,7 +48,46 @@ def find_by_tag
   if @keyword == ''
     @recipes = Recipe.none
   else
-    @recipes = Recipe.tagged_with(params[:search])
+    # @recipes = Recipe.tagged_with(params[:search])
+
+    @tag_id = ActsAsTaggableOn::Tag.find_by_sql(["
+    SELECT tags.id FROM tags WHERE (LOWER(name) = LOWER(?))
+    ", @keyword])
+
+    puts @tag
+
+    # @tag.blank?
+    @recipes = Recipe.find_by_sql(["
+    SELECT recipes_with_tag.* , 
+       COALESCE( like.cnt, 0 ) AS users_liking_it_count,
+       COALESCE( bookmark.cnt, 0 ) AS users_bookmarking_it_count,
+       COALESCE( cook.cnt, 0 ) AS cooks_count
+    FROM (
+      SELECT * FROM 
+      recipes
+      JOIN taggings ON taggings.taggable_id = recipes.id
+      AND taggings.taggable_type = 'Recipe'
+      AND taggings.tag_id = ?
+    ) AS recipes_with_tag
+    LEFT JOIN 
+      ( SELECT recipe_id, COUNT(*) AS cnt FROM
+        user_like_recipes
+        GROUP BY recipe_id ) AS like
+    ON recipes_with_tag.id = like.recipe_id
+    LEFT JOIN
+      ( SELECT recipe_id, COUNT(*) AS cnt FROM
+        user_bookmark_recipes
+        GROUP BY recipe_id ) AS bookmark
+    ON recipes_with_tag.id = bookmark.recipe_id
+    LEFT JOIN
+      ( SELECT recipe_id, COUNT(*) AS cnt FROM
+        masterpieces
+        GROUP BY recipe_id ) AS cook
+    ON recipes_with_tag.id = cook.recipe_id
+    ", @tag_id])
+
+    # ActiveRecord::Associations::Preloader.new.preload(@recipes, :tags)
+    # ActiveRecord::Associations::Preloader.new.preload(@recipes, :creator)
   end
 end
 
