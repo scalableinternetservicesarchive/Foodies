3ac43590e4b57171d43543672b58deb9640fe2d7
muxin <muxin4ever@gmail.com>
2015-11-21 22:07:08 -0800

    Fragment caching for homepage: cache recipe table

--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -2,4 +2,9 @@ module HomepageHelper
   def cache_key_for_recipe_snippet(recipe)
     "homepage-recipe-#{recipe.id}-#{recipe.updated_at}"
   end
+
+  def cache_key_for_recipe_table
+    max_updated_at = @top_bookmarked_recipes.max_by(&:updated_at).updated_at.try(:utc).try(:to_s, :number)
+    "homepage-recipe-table-#{max_updated_at}"
+  end
 end
