ae1c586cb76e7f107e9b568137e41060817af635
zjciris <zjc9211@gmail.com>
2015-11-24 22:15:11 -0800

    Server side caching fragment caching

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,8 @@
 module SearchHelper
+  def cache_key_for_search_recipe_snippet(recipe)
+    "recipe-#{recipe.id}-#{recipe.updated_at}-#{recipe.users_liking_it.count}-#{recipe.users_bookmarking_it.count}-#{recipe.cooks.count}"
+  end
+  def cache_key_for_search_user_snippet(user)
+    "user-#{user.id}-#{user.updated_at}-#{user.username}-#{user.email}"
+  end
 end
