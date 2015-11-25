1d7c8efe3b20e84e654ace48d1ae8bb1040e39f6
rigenmu <rigenmu@gmail.com>
2015-11-24 21:01:51 -0800

    Server side caching fragment caching recipe show

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,3 +1,14 @@
 module RecipesHelper
   include ActsAsTaggableOn::TagsHelper
+  def cache_key_for_masterpiece_snippet(masterpiece)
+    "masterpiece-#{masterpiece.id}-#{masterpiece.updated_at.try(:utc).try(:to_s, :number)}"
+  end
+
+  # def cache_key_for_recipe_show_part1(recipe)
+  #   "recipe-show-part1-#{recipe.id}-#{recipe.updated_at.try(:utc).try(:to_s, :number)}"
+  # end
+  #
+  # def cache_key_for_recipe_show_part2(recipe)
+  #   "recipe-show-part2-#{recipe.id}-#{recipe.updated_at.try(:utc).try(:to_s, :number)}"
+  # end
 end
