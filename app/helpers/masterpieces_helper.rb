13de23a45163d7b8a36ceed8ad3ac9b1d6635d11
ZQY <sunnyuny8@gmail.com>
2015-11-24 21:36:00 -0800

    Server side caching fragment caching masterpiece show view

--- app/helpers/masterpieces_helper.rb
+++ app/helpers/masterpieces_helper.rb
@@ -1,2 +1,6 @@
 module MasterpiecesHelper
+  def cache_key_for_masterpiece_show(masterpiece)
+    max_users_liking_it_update = masterpiece.users_liking_it.max_by(&:updated_at).updated_at.try(:utc).try(:to_s, :number)
+    "masterpiece-show-#{masterpiece.id}-#{masterpiece.updated_at.try(:utc).try(:to_s, :number)}-#{masterpiece.recipe.updated_at.try(:utc).try(:to_s, :number)}-#{current_user.id if current_user}-#{max_users_liking_it_update}"
+  end
 end
