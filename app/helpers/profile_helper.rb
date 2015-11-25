f3185a38ebaf146a71b7f1f32a3ac28214d7b446
katiesl <anvincy@gmail.com>
2015-11-24 22:17:10 -0800

    server side fragment caching for profile page

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,2 +1,17 @@
 module ProfileHelper
+	def cache_key_for_profile(user)
+		"user-#{user.id}-#{user.updated_at}-#{user.email}"
+	end
+	# def cache_key_for_profile_bookmarked(user)
+	# 	"user-#{user.id}-#{user.updated_at}-#{user.}"
+	# end
+	# def cache(cache_key_for_profile_liked(user)
+	# 	"user-#{user.id}-#{user.updated_at}-#{user.}"
+	# end
+	# def cache_key_for_profile_cooked(user)
+	# 	"user-#{user.id}-#{user.updated_at}-#{user.}"
+	# end
+	# def cache_key_for_profile_liked_masterpiece(user)
+	# 	"user-#{user.id}-#{user.updated_at}-#{user.}"
+	# end
 end
