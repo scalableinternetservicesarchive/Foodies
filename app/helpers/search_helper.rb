3174b92fc88b16b7f5e38790b3ac9e698b9babb7
zjciris <zjc9211@gmail.com>
2015-11-26 16:31:18 -0800

    cache_key_for_search_recipe_snippet:utc

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,8 +1,8 @@
 module SearchHelper
   def cache_key_for_search_recipe_snippet(recipe)
-    "recipe-#{recipe.id}-#{recipe.updated_at}-#{recipe.users_liking_it.count}-#{recipe.users_bookmarking_it.count}-#{recipe.cooks.count}"
+    "recipe-#{recipe.id}-#{recipe.updated_at.try(:utc).try(:to_s,:number)}-#{recipe.users_liking_it.count}-#{recipe.users_bookmarking_it.count}-#{recipe.cooks.count}"
   end
   def cache_key_for_search_user_snippet(user)
-    "user-#{user.id}-#{user.updated_at}-#{user.username}-#{user.email}"
+    "user-#{user.id}-#{user.updated_at.try(:utc).try(:to_s,:number)}"
   end
 end
